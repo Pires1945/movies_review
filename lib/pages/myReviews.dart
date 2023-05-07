c9a06c4581573bcc8efea61377719662d9e91036
Pires1945 <felipe.madeinbrazil@gmail.com>
2023-05-07 13:23:18 -0300

    tela de minhas reviews

--- lib/pages/myReviews.dart
+++ lib/pages/myReviews.dart
@@ -39,15 +39,29 @@ class _MyReviewsState extends State<MyReviews> {
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedMyReviews.length.toString()),
+        backgroundColor: Color.fromARGB(255, 20, 20, 20),
       ),
-      body: ListView.builder(
-        itemCount: loadedMyReviews.length,
-        padding: const EdgeInsets.only(bottom: 60),
-        itemBuilder: (context, index) => ChangeNotifierProvider.value(
-          value: loadedMyReviews[index],
-          child: Padding(
-            padding: const EdgeInsets.all(3.0),
-            child: Text(loadedMyReviews[index].userId),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 9, 85, 99),
+              Color.fromARGB(255, 32, 32, 32),
+              Colors.black,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: ListView.builder(
+          itemCount: loadedMyReviews.length,
+          padding: const EdgeInsets.only(bottom: 60),
+          itemBuilder: (context, index) => ChangeNotifierProvider.value(
+            value: loadedMyReviews[index],
+            child: Padding(
+              padding: const EdgeInsets.all(3.0),
+              child: ReviewItem(loadedMyReviews[index]),
+            ),
           ),
         ),
       ),
